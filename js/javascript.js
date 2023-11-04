f710863ce1525e8005f193c90a921f966361b3c3
Andy Garcia <barcag07@icloud.com>
2023-11-04 16:35:09 -0500

    Create function of game, determine results, and show winner

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,82 @@
+
 //Create function to randomly decide computer choice
 function getComputerChoice() {
     //use Math.floor to round number down to nearest integer
     //Multiply Math.random by 3 so that the values are [0,3)
     let num = Math.floor(Math.random() * 3);
-    let decision;
+    let compDecision;
     switch (num) {
-        case 0 : decision = "Rock";
+        case 0 : compDecision = "Rock";
         break;
 
-        case 1: decision = "Paper";
+        case 1: compDecision = "Paper";
         break;
 
-        case 2: decision = "Scissors";
+        case 2: compDecision = "Scissors";
         break;
     }
 
-    return decision;
+    return compDecision;
 }
+
+let computer = getComputerChoice();
 
 
 
 //ask the user for their input
 function getUserChoice() {
-    let choice = prompt("What do you pick? Rock, Paper, Scissors");
-console.log("User: " + choice);
+    let userChoice = prompt("What do you pick? Rock, Paper, Scissors");
+    return userChoice;
 }
 
-getUserChoice();
-console.log("Computer: " + getComputerChoice());
+let user = getUserChoice();
+
+//Create function of game (rps) that uses values from computer and user variable
+function rps(computer, user) {
+    console.log("Computer: " + computer);
+    console.log("User: " + user);
+}
+
+//calls function of rps to display results with values from computer and user values
+rps(computer, user);
+
+//Determines winner
+if (computer == "Rock" && user == "Rock") {
+    
+    console.log("It's a tie. Nobody wins.");
+}
+
+else if (computer == "Rock" && user == "Paper") {
+    console.log("Congratulations! You beat the computer!");
+}
+
+else if (computer == "Rock" && user == "Scissors") {
+    console.log("Sorry the computer wins this game.");
+}
+
+else if (computer == "Paper" && user == "Rock") {
+    
+    console.log("Sorry the computer wins this game.");
+}
+
+else if (computer == "Paper" && user == "Paper") {
+    console.log("It's a tie. Nobody wins.");
+}
+
+else if (computer == "Paper" && user == "Scissors") {
+    console.log("Congratulations! You beat the computer!");
+}
+
+if (computer == "Scissors" && user == "Rock") {
+    
+    console.log("Congratulations! You beat the computer!");
+}
+
+else if (computer == "Scissors" && user == "Paper") {
+    console.log("Sorry the computer wins this game.");
+}
+
+else if (computer == "Scissors" && user == "Scissors") {
+    console.log("It's a tie. Nobody wins.");
+}
+
