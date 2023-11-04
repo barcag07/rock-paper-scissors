f227f3fc310d5858e3d21e127b49d9889eaeb0d0
Andy Garcia <barcag07@icloud.com>
2023-11-04 01:45:24 -0500

    Create the function that randomizes the computer's decision

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+function computerChoice() {
+    //use Math.floor to round number down to nearest integer
+    //Multiply Math.random by 3 so that the values are [0,3)
+    let num = Math.floor(Math.random() * 3);
+    let decision;
+    switch (num) {
+        case 0 : decision = "Rock";
+        break;
+
+        case 1: decision = "Paper";
+        break;
+
+        case 2: decision = "Scissors";
+        break;
+    }
+
+    return decision;
+}
+
+console.log(computerChoice());
